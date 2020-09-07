a8077e5984d3924034f7d36ba5827cbb9c2471b6
coderjaymoh <lelerukjaymoh@gmail.com>
2020-09-07 11:41:50 +0300

    Redesigning portifolio

--- portifolio/urls.py
+++ portifolio/urls.py
@@ -3,4 +3,5 @@ from portifolio import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('blog', views.blog, name='blog'),
 ]
--- portifolio/views.py
+++ portifolio/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    return render(request, 'index.html', {})
 
+def blog(request):
+    return render(request, 'blog.html', {})
+
+def blog_post(request, pk):
+    return render(request, 'details.html', {})
+
+def projects(request, pk):
+    return render(request, 'details.html', {})
+
+def project_details(request, pk):
+    return render(request, 'details.html', {})
