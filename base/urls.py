15c322b725ea5ffae7c64c52560f8805ae31a9ec
Jaymoh <lelerukjaymoh@gmail.com>
2021-01-27 01:43:41 +0300

    settings for serving static files

--- base/urls.py
+++ base/urls.py
@@ -5,3 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portifolio.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
